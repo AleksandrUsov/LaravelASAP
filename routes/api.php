b1fe21782f8e035cf8ca43fe63372ed968b2c5a5
Aleksandr Usov <usov.als579@gmail.com>
2023-05-31 21:45:23 +0700

    Разбор темы API Resources

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Resources\PostCollection;
+use App\Http\Resources\PostResource;
+use App\Http\Resources\UserCollection;
+use App\Http\Resources\UserResource;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +23,32 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('/users')->group( function () {
+    Route::get('/user/{id}', function ($id) {
+        return new UserResource(User::query()->find($id));
+    });
+
+    Route::get('/all', function () {
+        return UserResource::collection(User::all()->keyBy->id);
+    });
+
+    Route::get('/collection', function () {
+        return new UserCollection(User::all());
+    });
+});
+
+Route::prefix('/posts')->group(function () {
+    Route::get('/post/{id}', function ($id) {
+        return new PostResource(Post::query()->find($id));
+    });
+
+    Route::get('/all', function () {
+        return PostResource::collection(Post::all());
+    });
+
+    Route::get('/collection', function () {
+        return new PostCollection(Post::all());
+    });
+});
+
